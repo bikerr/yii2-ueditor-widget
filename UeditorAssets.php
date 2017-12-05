94dd37643f1bc8bc1784abff6e08d6d99d23c2a6
biker <ujschenlin@gmail.com>
2017-12-05 11:40:23 +0800

    获取返回图片大小 添加手机预览 修改展示样式

---
+++ UeditorAssets.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace xj\ueditor;
+
+use Yii;
+use yii\web\AssetBundle;
+
+class UeditorAssets extends AssetBundle {
+
+    public $sourcePath = '@vendor/xj/yii2-ueditor-widget/assets';
+    public $basePath = '@webroot/assets';
+    public $css = [
+        'css/editor.css',
+    ];
+    public $js = [
+        'ueditor.config.js',
+        'ueditor.all.js',
+    ];
+    public $depends = ['yii\web\JqueryAsset'];
+
+}
